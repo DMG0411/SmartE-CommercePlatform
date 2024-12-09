b1681179047b15877ad1f1c01061fbef72bcde83
DMG0411 <m2bobko@gmail.com>
2024-12-10 01:31:10 +0200

    Updated.

--- Frontend/smart-ecommerce-platform/src/app/features/services/cart.service.ts
+++ Frontend/smart-ecommerce-platform/src/app/features/services/cart.service.ts
@@ -2,7 +2,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Cart } from '../models';
 import { BehaviorSubject, Observable } from 'rxjs';
-import { environment } from '@app/environment';
+import { environment } from 'environments/environment';
 
 @Injectable({
   providedIn: 'root',
