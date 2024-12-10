1777f37ab6bbd6e01b0944b68d860d89dc8f013d
DMG0411 <m2bobko@gmail.com>
2024-12-10 02:01:08 +0200

    Deploy. Auth guard fixed.

--- Frontend/smart-ecommerce-platform/src/app/shared/utils/auth-guard.ts
+++ Frontend/smart-ecommerce-platform/src/app/shared/utils/auth-guard.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { CanActivateFn, Router, UrlTree } from '@angular/router';
+import { environment } from 'environments/environment';
 import { ToastrService } from 'ngx-toastr';
 import { catchError, map, Observable, of } from 'rxjs';
 
@@ -9,7 +10,7 @@ export const authGuard: CanActivateFn = (): Observable<boolean | UrlTree> => {
   const http: HttpClient = inject(HttpClient);
   const toastrService: ToastrService = inject(ToastrService);
 
-  const url: string = 'https://localhost:7078/api/v1/user/checkToken';
+  const url: string = `${environment}/user/checkToken`;
 
   return http.get<void>(url).pipe(
     map(() => true),
